e4db23bba7b91e9c9e0dd0663b4adf18699b9b75
caoyingjun <cao.yingjunz@gmail.com>
2021-03-06 20:18:44 +0800

    Use cobra to start the deamon
    Change-Id: I0a81120d26248ff9a8431aae83c357cc8b6c7025

---
+++ cmd/app/autoscaler.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog"
+
+	"github.com/caoyingjunz/kubez-autoscaler/pkg/controller"
+	"github.com/caoyingjunz/kubez-autoscaler/pkg/controller/autoscaler"
+)
+
+const (
+	workers = 5
+)
+
+// NewAutoscalerCommand creates a *cobra.Command object with default parameters
+func NewAutoscalerCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "kubez-autoscaler",
+		Long: `The kubez autoscaler manager is a daemon than embeds
+the core control loops shipped with advanced HPA.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+		},
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+			return nil
+		},
+	}
+	return cmd
+}
+
+func Run() error {
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	var config *rest.Config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		klog.Warning("Geting config from In-Cluster failed, Try fetching config from HomeDir")
+		config, err = clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
+		if err != nil {
+			return err
+		}
+	}
+
+	rootClientBuilder := controller.SimpleControllerClientBuilder{
+		ClientConfig: config,
+	}
+
+	versionedClient := rootClientBuilder.ClientOrDie("shared-informers")
+	InformerFactory := informers.NewSharedInformerFactory(versionedClient, time.Minute)
+
+	ac, err := autoscaler.NewAutoscalerController(
+		InformerFactory.Apps().V1().Deployments(),
+		InformerFactory.Autoscaling().V1().HorizontalPodAutoscalers(),
+		rootClientBuilder.ClientOrDie("shared-informers"),
+	)
+	if err != nil {
+		return err
+	}
+	go ac.Run(workers, stopCh)
+
+	select {}
+
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -17,113 +17,18 @@ limitations under the License.
 package main
 
 import (
-	"flag"
-	"path/filepath"
+	"math/rand"
+	"os"
 	"time"
 
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/informers"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"k8s.io/klog"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	"github.com/caoyingjunz/kubez-autoscaler/pkg/controller"
-	"github.com/caoyingjunz/kubez-autoscaler/pkg/controller/autoscaler"
+	"github.com/caoyingjunz/kubez-autoscaler/cmd/app"
 )
-
-const (
-	workers = 5
-)
-
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
-	_ = appsv1.AddToScheme(scheme)
-}
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "0a8573fa.github.com",
-	})
-	if err != nil {
-		klog.Fatal("start controller failed: %v", err)
-	}
-
-	stopCh := make(chan struct{})
-	defer close(stopCh)
-
-	// TODO: build kube config
-	var config *rest.Config
-	config, err = rest.InClusterConfig()
-	if err != nil {
-		klog.Warning("Geting config from In-Cluster failed, Try fetching config from HomeDir")
-		config, err = clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
-		if err != nil {
-			klog.Fatal(err)
-		}
-	}
-
-	rootClientBuilder := controller.SimpleControllerClientBuilder{
-		ClientConfig: config,
-	}
-
-	versionedClient := rootClientBuilder.ClientOrDie("shared-informers")
-	InformerFactory := informers.NewSharedInformerFactory(versionedClient, time.Minute)
-
-	ac, err := autoscaler.NewAutoscalerController(
-		InformerFactory.Apps().V1().Deployments(),
-		InformerFactory.Autoscaling().V1().HorizontalPodAutoscalers(),
-		rootClientBuilder.ClientOrDie("shared-informers"),
-	)
-	if err != nil {
-		klog.Fatal(err)
-	}
-	go ac.Run(workers, stopCh)
-
-	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
-		klog.Fatal("set up health check failed: %v", err)
-	}
-	if err := mgr.AddReadyzCheck("check", healthz.Ping); err != nil {
-		klog.Fatal("set up ready check failed: %v", err)
-	}
-
-	klog.Infof("Starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		klog.Fatal("running manager failed: %v", err)
+	command := app.NewAutoscalerCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
 	}
 }
