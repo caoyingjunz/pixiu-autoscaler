c4b44d8f3840a97328f93de74460f315cf3d61d6
caoyingjun <cao.yingjunz@gmail.com>
2021-03-06 16:22:09 +0800

    Completed HPA for deployment by informer
    Change-Id: I8273409ecd491c3b40a63dffe9d280da739f483e

--- pkg/controller/autoscaler/autoscaler.go
+++ pkg/controller/autoscaler/autoscaler.go
@@ -19,9 +19,11 @@ package autoscaler
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"strconv"
 	"time"
 
+	apps "k8s.io/api/apps/v1"
 	autoscalingv2 "k8s.io/api/autoscaling/v2beta2"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -30,10 +32,12 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
 	autoscalinginformers "k8s.io/client-go/informers/autoscaling/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	appslisters "k8s.io/client-go/listers/apps/v1"
 	autoscalinglisters "k8s.io/client-go/listers/autoscaling/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
@@ -59,12 +63,16 @@ type AutoscalerController struct {
 	hpaLister       autoscalinglisters.HorizontalPodAutoscalerLister
 	hpaListerSynced cache.InformerSynced
 
+	// dLister can list/get deployments from the shared informer's store
+	dLister       appslisters.DeploymentLister
+	dListerSynced cache.InformerSynced
+
 	// KubezController that need to be synced
 	queue workqueue.RateLimitingInterface
 }
 
 // NewAutoscalerController creates a new AutoscalerController.
-func NewAutoscalerController(hpaInformer autoscalinginformers.HorizontalPodAutoscalerInformer, client clientset.Interface) (*AutoscalerController, error) {
+func NewAutoscalerController(dInformer appsinformers.DeploymentInformer, hpaInformer autoscalinginformers.HorizontalPodAutoscalerInformer, client clientset.Interface) (*AutoscalerController, error) {
 	eventBroadcaster := record.NewBroadcaster()
 	eventBroadcaster.StartLogging(klog.Infof)
 	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: client.CoreV1().Events("")})
@@ -81,17 +89,27 @@ func NewAutoscalerController(hpaInformer autoscalinginformers.HorizontalPodAutos
 		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "autoscaler"),
 	}
 
+	// HorizontalPodAutoscaler
 	hpaInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    ac.addHPA,
 		UpdateFunc: ac.updateHPA,
 		DeleteFunc: ac.deleteHPA,
 	})
-
-	ac.syncHandler = ac.syncAutoscalers
-	ac.enqueueHPA = ac.enqueue
-
 	ac.hpaLister = hpaInformer.Lister()
 	ac.hpaListerSynced = hpaInformer.Informer().HasSynced
+
+	// Deployment
+	dInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    ac.addDeployment,
+		UpdateFunc: ac.updateDeployment,
+		DeleteFunc: ac.deleteDeployment,
+	})
+	ac.dLister = dInformer.Lister()
+	ac.dListerSynced = dInformer.Informer().HasSynced
+
+	// syncAutoscalers
+	ac.syncHandler = ac.syncAutoscalers
+	ac.enqueueHPA = ac.enqueue
 
 	return ac, nil
 }
@@ -117,6 +135,14 @@ func (ac *AutoscalerController) Run(workers int, stopCh <-chan struct{}) {
 		DeleteFunc: ac.deleteHPA,
 	})
 	go informer.Run(stopCh)
+
+	dInformer := sharedInformers.Apps().V1().Deployments().Informer()
+	dInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    ac.addDeployment,
+		UpdateFunc: ac.updateDeployment,
+		DeleteFunc: ac.deleteDeployment,
+	})
+	go dInformer.Run(stopCh)
 
 	<-stopCh
 }
@@ -155,6 +181,104 @@ func (ac *AutoscalerController) deleteHPA(obj interface{}) {
 	//ac.enqueueHPA(h)
 
 	ac.handerHPADeleteEvent(h)
+}
+
+func (ac *AutoscalerController) addDeployment(obj interface{}) {
+	d := obj.(*apps.Deployment)
+	klog.V(0).Infof("Adding Deployment %s/%s", d.Namespace, d.Name)
+
+	maxReplicas, ok := d.Annotations[controller.MaxReplicas]
+	if !ok {
+		// return directly
+		return
+	}
+
+	maxReplicasInt, err := strconv.ParseInt(maxReplicas, 10, 32)
+	if err != nil || maxReplicasInt == 0 {
+		return
+	}
+	hpa := controller.CreateHorizontalPodAutoscaler(d.Name, d.Namespace, d.UID, "apps/v1", "Deployment", int32(maxReplicasInt))
+	klog.Infof("Reconciling HPA %s/%s from %s", d.Namespace, d.Name, "Deployment")
+	_, err = ac.client.AutoscalingV2beta2().HorizontalPodAutoscalers(d.Namespace).Create(context.TODO(), hpa, metav1.CreateOptions{})
+	if err != nil {
+		if errors.IsAlreadyExists(err) {
+			return
+		}
+		klog.Info(err)
+	}
+}
+
+func (ac *AutoscalerController) updateDeployment(old, current interface{}) {
+	oldD := old.(*apps.Deployment)
+	curD := current.(*apps.Deployment)
+	klog.V(0).Infof("Updating Deployment %s/%s", oldD.Namespace, curD.Name)
+
+	oldAnnotations := oldD.Annotations
+	curAnnotations := curD.Annotations
+	if reflect.DeepEqual(oldAnnotations, curAnnotations) {
+		return
+	}
+
+	oldMaxReplicas, oldOk := oldAnnotations[controller.MaxReplicas]
+	curMaxReplicas, curOk := curAnnotations[controller.MaxReplicas]
+
+	if oldOk && curOk {
+		if oldMaxReplicas != curMaxReplicas {
+			// UPDATE
+			maxReplicasInt, err := strconv.ParseInt(curMaxReplicas, 10, 32)
+			if err != nil || maxReplicasInt == 0 {
+				klog.Errorf("maxReplicas is requred")
+				return
+			}
+			hpa, err := ac.client.AutoscalingV2beta2().
+				HorizontalPodAutoscalers(oldD.Namespace).
+				Get(context.TODO(), oldD.Name, metav1.GetOptions{})
+			if err != nil {
+				// HPA 不存在
+				curHpa := controller.CreateHorizontalPodAutoscaler(curD.Name, curD.Namespace, curD.UID, "apps/v1", "Deployment", int32(maxReplicasInt))
+				_, err := ac.client.AutoscalingV2beta2().
+					HorizontalPodAutoscalers(curD.Namespace).
+					Create(context.TODO(), curHpa, metav1.CreateOptions{})
+				klog.Error(err)
+				return
+			}
+			hpa.Spec.MaxReplicas = int32(maxReplicasInt)
+			_, err = ac.client.AutoscalingV2beta2().HorizontalPodAutoscalers(curD.Namespace).Update(context.TODO(), hpa, metav1.UpdateOptions{})
+			if err != nil {
+				klog.Error(err)
+			}
+		}
+	} else if oldOk && !curOk {
+		// DELETE HPA
+		ac.client.AutoscalingV2beta2().HorizontalPodAutoscalers(oldD.Namespace).Delete(context.TODO(), oldD.Name, metav1.DeleteOptions{})
+	} else if !oldOk && curOk {
+		// CREATE HPA
+		maxReplicasInt, err := strconv.ParseInt(curMaxReplicas, 10, 32)
+		if err != nil || maxReplicasInt == 0 {
+			klog.Errorf("maxReplicas is requred")
+			return
+		}
+		hpa := controller.CreateHorizontalPodAutoscaler(curD.Name, curD.Namespace, curD.UID, "apps/v1", "Deployment", int32(maxReplicasInt))
+		_, err = ac.client.AutoscalingV2beta2().
+			HorizontalPodAutoscalers(curD.Namespace).
+			Create(context.TODO(), hpa, metav1.CreateOptions{})
+		if err != nil {
+			klog.Error(err)
+		}
+	}
+}
+
+func (ac *AutoscalerController) deleteDeployment(obj interface{}) {
+	d := obj.(*apps.Deployment)
+	klog.V(0).Infof("Deleting Deployment %s/%s", d.Namespace, d.Name)
+
+	_, ok := d.Annotations[controller.MaxReplicas]
+	if ok {
+		klog.Infof("The HPA %s/%s is reconciling(DELETE)", d.Namespace, d.Name)
+		if err := ac.client.AutoscalingV2beta2().HorizontalPodAutoscalers(d.Namespace).Delete(context.TODO(), d.Name, metav1.DeleteOptions{}); err != nil {
+			klog.Error(err)
+		}
+	}
 }
 
 // KubeAutoscaler is responsible for HPA objects stored.
