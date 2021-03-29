c25685d4ea6f55b301cd23f4586b8a31c2b4b721
caoyingjun <cao.yingjun@foxmail.com>
2021-03-30 00:23:33 +0800

    Fix duplicated Add hpa handler
    Change-Id: Iecd24d45936d06dd2ce622f634e8d0a9e360040a

--- pkg/controller/autoscaler/autoscaler.go
+++ pkg/controller/autoscaler/autoscaler.go
@@ -457,6 +457,17 @@ func (ac *AutoscalerController) addHPA(obj interface{}) {
 	if !controller.ManagerByKubezController(h) {
 		return
 	}
+
+	oldH, err := ac.hpaLister.HorizontalPodAutoscalers(h.Namespace).Get(h.Name)
+	if err != nil && !errors.IsNotFound(err) {
+		klog.Warningf("Failed to get %s/%s for %s", h.Namespace, h.Name, h.Spec.ScaleTargetRef.Kind)
+	}
+	if h.ResourceVersion == oldH.ResourceVersion {
+		// Periodic resync will send update events for all known HPAs.
+		// Two different versions of the same HPA will always have different ResourceVersions.
+		return
+	}
+
 	klog.V(0).Infof("Adding HPA(manager by kubez) %s/%s", h.Namespace, h.Name)
 }
 
@@ -551,25 +562,31 @@ func (ac *AutoscalerController) handleErr(err error, key interface{}) {
 
 // This functions just wrap HandlerEvents for improve the readability of codes
 func (ac *AutoscalerController) addDeployment(obj interface{}) {
+	klog.Infof("Handering add Deployment event")
 	ac.HandlerAddEvents(obj)
 }
 
 func (ac *AutoscalerController) updateDeployment(old, cur interface{}) {
+	klog.Infof("Handering update Deployment event")
 	ac.HandlerUpdateEvents(old, cur)
 }
 
 func (ac *AutoscalerController) deleteDeployment(obj interface{}) {
+	klog.Infof("Handering delete Deployment event")
 	ac.HandlerDeleteEvents(obj)
 }
 
 func (ac *AutoscalerController) addStatefulset(obj interface{}) {
+	klog.Infof("Handering add StatefulSet event")
 	ac.HandlerAddEvents(obj)
 }
 
 func (ac *AutoscalerController) updateStatefulset(old, cur interface{}) {
+	klog.Infof("Handering update StatefulSet event")
 	ac.HandlerUpdateEvents(old, cur)
 }
 
 func (ac *AutoscalerController) deleteStatefulset(obj interface{}) {
+	klog.Infof("Handering delete StatefulSet event")
 	ac.HandlerDeleteEvents(obj)
 }
